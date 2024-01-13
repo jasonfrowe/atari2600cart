09e9984e9131b425e3aaa116226c5f0e2d808d58
Jason Rowe <jasonfrowe@gmail.com>
2024-01-13 17:31:28 -0500

    better ROM imports
    much faster compiles and uses a lot less flash space

--- rom.c
+++ rom.c
@@ -1,24 +1,26 @@
 /*
-* PicoROM
-* Simulate a ROM chip (e.g. 28c256) with a Raspberry Pi Pico.
-* Nick Bild (nick.bild@gmail.com)
-* August 2021
+* PicoMultiRom for ATARI 2600+
+* Jason Rowe (jasonfrowe@gmail.com)
+* Based on PicoROM by Nick Bild (nick.bild@gmail.com)
+* and Otaku-flash by karrika (https://github.com/karrika/Otaku-flash)
+* January 2024
 */
 
 #include "pico/stdlib.h"
 #include "hardware/clocks.h"
 #include "hardware/vreg.h"
 #include "pin_definitions.h"
-#include "romheader.h"
+// #include "romheader.h"
 #include <stdlib.h>
 #include "hardware/adc.h"
 
 void setup_gpio();
 int get_requested_address();
-void put_data_on_bus(int);
+void put_data_on_bus(uint8_t);
 void setup_rom_contents();
 void data_bus_input();
 void data_bus_output();
+void setup_cart();
 
 uint romsize = 4096;
 uint8_t rom_contents[32768] = { 0 };
@@ -31,22 +33,6 @@ uint16_t romoffset = 0;
 uint16_t ce;
 uint16_t last_ce;
 
-void roms_00();
-void roms_01();
-void roms_02();
-void roms_03();
-void roms_04();
-void roms_05();
-void roms_06();
-void roms_07();
-void roms_08();
-void roms_09();
-void roms_10();
-void roms_11();
-void roms_12();
-void roms_13();
-void roms_14();
-void roms_15();
 
 int main() {
 
@@ -69,83 +55,8 @@ int main() {
     gpio_set_dir(CE_PIN, GPIO_IN);
     // gpio_pull_up(CE_PIN);
 
-    // Read in DIPs to get cart to use
-    gpio_init(20);
-    gpio_set_dir(20, GPIO_IN);
-    if (gpio_get(20)){
-        cart+=1;
-    } 
-
-    gpio_init(21);
-    gpio_set_dir(21, GPIO_IN);
-    if (gpio_get(21)){
-        cart+=2;
-    }
-
-    gpio_init(22);
-    gpio_set_dir(22, GPIO_IN);
-    if (gpio_get(22)){
-        cart+=4;
-    }
-
-    // gpio_init(26);
-    // gpio_set_dir(26, GPIO_IN);
-    // if (gpio_get(26)){
-    //     cart+=8;
-    // }
-    adc_init();
-    adc_gpio_init(26);
-    adc_select_input(0);
-    // const float conversion_factor = 3.3f / (1 << 12);
-    uint16_t result = adc_read();
-    if (result  > 100){
-        cart+=8;
-    }
-
-    // Specify contents of emulated ROM.
-    // roms_01();
-    if (cart == 0){
-        roms_00();
-    } else if (cart == 1){
-        roms_01();
-    } else if (cart == 2){
-        roms_02();
-    } else if (cart == 3){
-        roms_03();
-    } else if (cart == 4){
-        roms_04();
-    } else if (cart == 5){
-        roms_05();
-    } else if (cart == 6){
-        roms_06();
-    } else if (cart == 7){
-        roms_07();
-    } else if (cart == 8){
-        roms_08();
-    } else if (cart == 9){
-        roms_09();
-    } else if (cart == 10){
-        roms_10();
-    } else if (cart == 11){
-        roms_11();
-    } else if (cart == 12){
-        roms_12();
-    } else if (cart == 13){
-        roms_13();
-    } else if (cart == 14){
-        roms_14();
-    } else if (cart == 15){
-        roms_15();
-    }
-
-    if (romsize == 8192){
-        bankswitch = 1; // F8 bankswitching
-    } else if (romsize == 16384){
-        bankswitch = 2; // F6 bankswitching
-    } else if (romsize == 32768){
-        bankswitch = 3; // F4 bankswitching
-    }
-
+    setup_cart();
+    
     last_ce = 1;
     
     // GPIO setup for address and data bus
@@ -154,20 +65,21 @@ int main() {
     // Continually check address lines and
     // put associated data on bus.
     while (true) {
-        // put_data_on_bus(get_requested_address());
 
         ce = gpio_get(CE_PIN);
         if (ce != last_ce) {
             last_ce = ce;
             if (last_ce) {
-                data_bus_input();
+                // data_bus_input();
+                gpio_set_dir_in_masked(0xFF);
             } else{
-                data_bus_output();
+                // data_bus_output();
+                gpio_set_dir_out_masked(0xFF);
             }
         }
         if (last_ce == 0){
             addr = get_requested_address();
-            put_data_on_bus(addr + romoffset);
+            put_data_on_bus(rom_contents[addr + romoffset]);
 
             if (bankswitch == 1 && addr == 4088){ //F8 bankswitching (8K)
                 romoffset = 0;
@@ -204,29 +116,149 @@ int main() {
     }
 }
 
-void data_bus_input() {
-    // Data pins.
-    gpio_set_dir(D0, GPIO_IN);
-    gpio_set_dir(D1, GPIO_IN);
-    gpio_set_dir(D2, GPIO_IN);
-    gpio_set_dir(D3, GPIO_IN);
-    gpio_set_dir(D4, GPIO_IN);
-    gpio_set_dir(D5, GPIO_IN);
-    gpio_set_dir(D6, GPIO_IN);
-    gpio_set_dir(D7, GPIO_IN);
+void setup_cart() {
+    // Read in DIPs to get cart to use
+    gpio_init(20);
+    gpio_set_dir(20, GPIO_IN);
+    if (gpio_get(20)){
+        cart+=1;
+    } 
+
+    gpio_init(21);
+    gpio_set_dir(21, GPIO_IN);
+    if (gpio_get(21)){
+        cart+=2;
+    }
+
+    gpio_init(22);
+    gpio_set_dir(22, GPIO_IN);
+    if (gpio_get(22)){
+        cart+=4;
+    }
+
+    // gpio_init(26);
+    // gpio_set_dir(26, GPIO_IN);
+    // if (gpio_get(26)){
+    //     cart+=8;
+    // }
+    adc_init();
+    adc_gpio_init(26);
+    adc_select_input(0);
+    // const float conversion_factor = 3.3f / (1 << 12);
+    uint16_t result = adc_read();
+    if (result  > 100){
+        cart+=8;
+    }
+
+    // Specify contents of emulated ROM.
+    if (cart == 0){
+        #include "roms_00.h"
+        romsize = romsize_in;
+        for (int i = 0; i < romsize_in; i++){
+            rom_contents[i] = rom_contents_in[i];
+        }
+    } else if (cart == 1){
+        #include "roms_01.h"
+        romsize = romsize_in;
+        for (int i = 0; i < romsize_in; i++){
+            rom_contents[i] = rom_contents_in[i];
+        }
+    } else if (cart == 2){
+        #include "roms_02.h"
+        romsize = romsize_in;
+        for (int i = 0; i < romsize_in; i++){
+            rom_contents[i] = rom_contents_in[i];
+        }
+    } else if (cart == 3){
+        #include "roms_03.h"
+        romsize = romsize_in;
+        for (int i = 0; i < romsize_in; i++){
+            rom_contents[i] = rom_contents_in[i];
+        }
+    } else if (cart == 4){
+        #include "roms_04.h"
+        romsize = romsize_in;
+        for (int i = 0; i < romsize_in; i++){
+            rom_contents[i] = rom_contents_in[i];
+        }
+    } else if (cart == 5){
+        #include "roms_05.h"
+        romsize = romsize_in;
+        for (int i = 0; i < romsize_in; i++){
+            rom_contents[i] = rom_contents_in[i];
+        }
+    } else if (cart == 6){
+        #include "roms_06.h"
+        romsize = romsize_in;
+        for (int i = 0; i < romsize_in; i++){
+            rom_contents[i] = rom_contents_in[i];
+        }
+    } else if (cart == 7){
+        #include "roms_07.h"
+        romsize = romsize_in;
+        for (int i = 0; i < romsize_in; i++){
+            rom_contents[i] = rom_contents_in[i];
+        }
+    } else if (cart == 8){
+        #include "roms_08.h"
+        romsize = romsize_in;
+        for (int i = 0; i < romsize_in; i++){
+            rom_contents[i] = rom_contents_in[i];
+        }
+    } else if (cart == 9){
+        #include "roms_09.h"
+        romsize = romsize_in;
+        for (int i = 0; i < romsize_in; i++){
+            rom_contents[i] = rom_contents_in[i];
+        }
+    } else if (cart == 10){
+        #include "roms_10.h"
+        romsize = romsize_in;
+        for (int i = 0; i < romsize_in; i++){
+            rom_contents[i] = rom_contents_in[i];
+        }
+    } else if (cart == 11){
+        #include "roms_11.h"
+        romsize = romsize_in;
+        for (int i = 0; i < romsize_in; i++){
+            rom_contents[i] = rom_contents_in[i];
+        }
+    } else if (cart == 12){
+        #include "roms_12.h"
+        romsize = romsize_in;
+        for (int i = 0; i < romsize_in; i++){
+            rom_contents[i] = rom_contents_in[i];
+        }
+    } else if (cart == 13){
+        #include "roms_13.h"
+        romsize = romsize_in;
+        for (int i = 0; i < romsize_in; i++){
+            rom_contents[i] = rom_contents_in[i];
+        }
+    } else if (cart == 14){
+        #include "roms_14.h"
+        romsize = romsize_in;
+        for (int i = 0; i < romsize_in; i++){
+            rom_contents[i] = rom_contents_in[i];
+        }
+    } else if (cart == 15){
+        #include "roms_15.h"
+        romsize = romsize_in;
+        for (int i = 0; i < romsize_in; i++){
+            rom_contents[i] = rom_contents_in[i];
+        }
+    }
+
+    if (romsize == 8192){
+        bankswitch = 1; // F8 bankswitching
+    } else if (romsize == 16384){
+        bankswitch = 2; // F6 bankswitching
+    } else if (romsize == 32768){
+        bankswitch = 3; // F4 bankswitching
+    }
+
 }
 
-void data_bus_output() {
-    // Data pins.
-    gpio_set_dir(D0, GPIO_OUT);
-    gpio_set_dir(D1, GPIO_OUT);
-    gpio_set_dir(D2, GPIO_OUT);
-    gpio_set_dir(D3, GPIO_OUT);
-    gpio_set_dir(D4, GPIO_OUT);
-    gpio_set_dir(D5, GPIO_OUT);
-    gpio_set_dir(D6, GPIO_OUT);
-    gpio_set_dir(D7, GPIO_OUT);
-}
 
 void setup_gpio() {
     // Address pins.
@@ -286,11 +318,11 @@ int get_requested_address() {
     return ((gpio_get_all() >> 8) & 4095);
 }
 
-void put_data_on_bus(int address) {
+void put_data_on_bus(uint8_t rom_cont) {
     // int data = rom_contents[address];
 
     // gpio mask = 8355840; // i.e.: 11111111000000000000000
     // Shift data 15 bits to put it in correct position to match data pin defintion.
-    gpio_put_masked(255, rom_contents[address]);
+    gpio_put_masked(255, rom_cont);
 }
 
